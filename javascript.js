b8a9d6f4aa2a2d5b8db7a818b8c57abd9eff0a8a
Marcus Cantu <cantumarcus19@gmail.com>
2018-05-01 17:02:16 -0600

    Added the good information that is needed.

--- javascript.js
+++ javascript.js
@@ -2,15 +2,9 @@ $(document).ready(function() {
 	$(window).scroll(function() {
   	if($(document).scrollTop() > 10) {
     	$('#nav').addClass('shrink');
-    	$('#picLogo').addClass('shrink');
-      $('.navButton').removeClass('btn btn-md ');
-      $('.navButton').addClass('btn btn-sm');
-
     }
     else {
     $('#nav').removeClass('shrink');
-    $('.navButton').removeClass('btn btn-sm');
-    $('.navButton').addClass('btn btn-lg');
     }
   });
 });
