099b2c2e4f78265314f182ee11d9860d3abefd48
Marcus Cantu <cantumarcus19@gmail.com>
2018-05-01 19:05:27 -0600

    Changed background colors and I also changed severl features.

--- javascript.js
+++ javascript.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
 	$(window).scroll(function() {
   	if($(document).scrollTop() > 10) {
     	$('#nav').addClass('shrink');
+    	$('#navStuff').addClass('shrink');
     }
     else {
     $('#nav').removeClass('shrink');
+    $('#navStuff').removeClass('shrink');
     }
   });
 });
